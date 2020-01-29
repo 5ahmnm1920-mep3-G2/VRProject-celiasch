f0b0b931dc51961d4e3841f9f4db5aedc980acc9
celiasch <clea.eliasch@htl-salzburg.ac.at>
2020-01-29 14:03:20 +0100

    Walking works now, started on working on an inventar

---
+++ Assets/MyGame/Scripts/PlayerGrab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrab : MonoBehaviour { 
+
+    public GameObject plasticCup;
+    public GameObject inventar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        plasticCup.transform.SetParent(inventar.transform);
+        plasticCup.transform.position = inventar.transform.position;
+    }
+}
