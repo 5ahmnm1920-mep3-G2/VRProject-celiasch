8bb7617c33f40b7b831fb6e5f5360575db093386
celiasch <clea.eliasch@htl-salzburg.ac.at>
2020-02-13 18:26:47 +0100

    Made Script Release and Pick up the Plastic Cup

--- Assets/MyGame/Scripts/PlayerGrab.cs
+++ Assets/MyGame/Scripts/PlayerGrab.cs
@@ -7,17 +7,35 @@ public class PlayerGrab : MonoBehaviour {
     public GameObject plasticCup;
     public GameObject inventar;
 
+    bool inHands = false;
+    Vector3 cupPos;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cupPos = plasticCup.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        plasticCup.transform.SetParent(inventar.transform);
-        plasticCup.transform.position = inventar.transform.position;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!inHands)
+            {
+                plasticCup.transform.SetParent(inventar.transform);
+                plasticCup.transform.localPosition = new Vector3(2.14f, 0.186f, 1.93f);
+                inHands = true;           
+            } else if (inHands)
+            {
+                this.GetComponent<PlayerGrab>().enabled = false;
+                plasticCup.transform.SetParent(null);
+                plasticCup.transform.localPosition = cupPos;
+                inHands = false;
+            }
+        }
+
     }
 }
+
