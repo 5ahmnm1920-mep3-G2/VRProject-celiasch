806fb12d9245f8e17e34057106daca9f31e62e8f
celiasch <clea.eliasch@htl-salzburg.ac.at>
2020-02-13 20:36:33 +0100

    included trees, included in script that tree appears when trash is gone, included Spot Lights, included Info UI Text

--- Assets/MyGame/Scripts/PlayerGrab.cs
+++ Assets/MyGame/Scripts/PlayerGrab.cs
@@ -6,15 +6,16 @@ public class PlayerGrab : MonoBehaviour {
 
     public GameObject plasticCup;
     public GameObject inventar;
+    public GameObject tree1;
 
     bool inHands = false;
-    Vector3 cupPos;
+    Vector3 binPos;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        cupPos = plasticCup.transform.position;
+        binPos = new Vector3(-1.26f, 0.1f, 0.02f);
     }
 
     // Update is called once per frame
@@ -25,17 +26,19 @@ public class PlayerGrab : MonoBehaviour {
             if (!inHands)
             {
                 plasticCup.transform.SetParent(inventar.transform);
-                plasticCup.transform.localPosition = new Vector3(-1.26f, 0.04f, 0.02f);
+                plasticCup.transform.localPosition = new Vector3(2.14f, 0.186f, 1.93f);
                 inHands = true;           
             } else if (inHands)
             {
                 this.GetComponent<PlayerGrab>().enabled = false;
                 plasticCup.transform.SetParent(null);
-                plasticCup.transform.localPosition = cupPos;
+                plasticCup.transform.localPosition = binPos;
+                tree1.transform.localPosition = new Vector3(2.14f, 0.186f, 1.93f);
                 inHands = false;
             }
         }
 
+    
     }
 }
 
